6bf5a2e9d8f0a133bc42084e2fc13b8e2b25447f
oguzhanselamoglu <oguzhanselamoglu@outlook.com>
2022-07-05 19:52:35 +0300

    variable

---
+++ GoFirstApp/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "değer"
+	var b = "string"
+	c := "string 3"
+	var d bool = true
+	var e int = 10
+	var f int = 20
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println("e + f = ", e+f)
+
+}
