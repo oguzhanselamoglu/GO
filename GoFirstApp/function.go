63b04c1f679f34ae4e12271db4d8df31b6844fba
oguzhan <oguzhanselamoglu@gmail.com>
2022-11-12 15:44:32 +0300

    function 2

--- GoFirstApp/function.go
+++ GoFirstApp/function.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func topla(a int, b int) int {
 	return a + b
@@ -13,6 +15,42 @@ func islenler(a, b int) (int, int, int, int) {
 	cikar := a - b
 	return toplam, carp, bol, cikar
 }
+
+func person(name string) func() {
+	_name := name
+	return func() {
+		fmt.Println(_name)
+	}
+}
+
+func person2(name string) func() string {
+	_name := name
+	persons := func() string {
+		return "değer " + _name
+	}
+	return persons
+}
+
+func count() func() {
+	n := 0
+	return func() {
+		n++
+		fmt.Println(n)
+	}
+}
+
+func count2() (first func(), second func() int) {
+	n := 0
+	first = func() {
+		n++
+		fmt.Println(n)
+	}
+	second = func() int {
+		return n
+	}
+	return
+}
+
 func main() {
 	t, c, b, d := islenler(8, 2)
 	_, e, _, _ := islenler(8, 2)
@@ -22,5 +60,17 @@ func main() {
 	fmt.Println("Bolme ", b)
 	fmt.Println("Çıkarma ", d)
 	fmt.Println("Sonuc ", e)
+	p := person("oguzhan")
+	p()
+	newPerson := person2("selamoglu")
+	f := newPerson()
 
+	fmt.Println(f)
+
+	//cnt := count()
+	first, second := count2()
+	for i := 0; i < 20; i++ {
+		first()
+	}
+	fmt.Println(second())
 }
