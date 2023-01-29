aaf8719fe115c5f9ab99e0b71acdc206eec91564
oguzhan <oguzhanselamoglu@outlook.com>
2023-01-29 13:35:28 +0300

    Response json

--- GOREST/pkg/api/api.go
+++ GOREST/pkg/api/api.go
@@ -1,22 +1,53 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+type Product struct {
+	Id   int
+	Name string
+}
+
+var products []Product
+
+func dataGet() {
+	prd1 := Product{
+		Id:   1,
+		Name: "PRD 1",
+	}
+	prd2 := Product{
+		Id:   2,
+		Name: "PRD 2",
+	}
+	products = append(products, prd1, prd2)
+}
+
 func Api() {
 	fmt.Println("Api")
 	r := mux.NewRouter()
-
+	dataGet()
 	r.HandleFunc("/", index)
 	r.HandleFunc("/post", post)
 	r.HandleFunc("/post/{category}/{id}", post)
+	r.HandleFunc("/products", getProducts)
 	http.ListenAndServe(":8080", r)
 
 }
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	j, err := json.Marshal(products)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "appliscation/json")
+	fmt.Fprintf(w, string(j))
+	//w.Write(j)
 
+}
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("index page"))
 }
