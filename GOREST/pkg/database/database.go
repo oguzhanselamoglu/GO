064fed1597c4ce9a18056cef33b7e757559a9342
oguzhan <oguzhanselamoglu@outlook.com>
2023-03-12 14:15:40 +0300

    insert

--- GOREST/pkg/database/database.go
+++ GOREST/pkg/database/database.go
@@ -10,9 +10,9 @@ var db *sql.DB
 
 func Database() {
 	fmt.Println("Database")
-
+	var err error
 	connStr := "postgres://veboni:Xidok4096H!@localhost/veboni-ae7949a8-7aa3-4038-b085-a98139bbe329?sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", connStr)
 
 	if err != nil {
 		panic(err)
@@ -21,18 +21,21 @@ func Database() {
 	if err = db.Ping(); err != nil {
 		panic(err)
 	}
+	addUser("oguzhan", "selamoglu", "oguzhan@kod.com.tr")
 
-	rows, err := db.Query(`SELECT "code" FROM "boards"`)
+	rows, err := db.Query(`SELECT "firstname","lastname","email" FROM "users"`)
 	CheckError(err)
 
 	defer rows.Close()
 	for rows.Next() {
-		var code string
+		var firstname, lastname, email string
 
-		err = rows.Scan(&code)
+		err = rows.Scan(&firstname, &lastname, &email)
 		CheckError(err)
 
-		fmt.Println(code)
+		fmt.Println(firstname)
+		fmt.Println(lastname)
+		fmt.Println(email)
 	}
 
 	CheckError(err)
@@ -42,4 +45,15 @@ func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func addUser(firstName, lastName, email string) {
+	stmt, err := db.Prepare(`insert into "users"("firstname","lastname","email","role_id","enterprises_id","authorizeenterprise","deleted","passive") values ($1, $2, $3, '46955c7a-a650-41f9-bad4-d2d8e130cb90','ae7949a8-7aa3-4038-b085-a98139bbe329',true,false,false) `)
+	CheckError(err)
+	res, err := stmt.Exec(firstName, lastName, email)
+	CheckError(err)
+	//	id, err := res.LastInsertId()
+	//	CheckError(err)
+	fmt.Println("Kayıtlı id", res)
+
 }
